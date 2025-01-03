556f2516f1e58e3d98724f3bbd37da66c195c574
Latheesan Kanesamoorthy <latheesan_k@hotmail.com>
2025-01-03 23:13:38 +0000

    interface Flesh out IPool interface

--- src/Contracts/IPool.php
+++ src/Contracts/IPool.php
@@ -4,5 +4,37 @@ namespace CardanoPhp\DataClient\Contracts;
 
 interface IPool
 {
-    // TODO
+    /**
+     * Return information about the requested stake pool.
+     *
+     * @param string $poolId
+     * @return array
+     */
+    public function poolInfo(string $poolId): array;
+
+    /**
+     * Return metadata for the requested stake pool.
+     *
+     * @param string $poolId
+     * @return array
+     */
+    public function poolMetadata(string $poolId): array;
+
+    /**
+     * Return relays for the requested stake pool.
+     *
+     * @param string $poolId
+     * @return array
+     */
+    public function poolRelays(string $poolId): array;
+
+    /**
+     * Return delegators for the requested stake pool.
+     *
+     * @param string $poolId
+     * @param int|null $pageNo
+     * @param int|null $resultsPerPage
+     * @return array
+     */
+    public function poolDelegators(string $poolId, int|null $pageNo, int|null $resultsPerPage): array;
 }
