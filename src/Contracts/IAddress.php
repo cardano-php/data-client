b91ab977a61f5107a0d4c68e01c8516778332b53
Latheesan Kanesamoorthy <latheesan_k@hotmail.com>
2025-01-03 02:30:28 +0000

    interface Flesh out IAddress interface

--- src/Contracts/IAddress.php
+++ src/Contracts/IAddress.php
@@ -4,5 +4,39 @@ namespace CardanoPhp\DataClient;
 
 interface IAddress
 {
-    // TODO
+    /**
+     * Return information about the requested address.
+     *
+     * @param string $address
+     * @return array
+     */
+    public function addressInfo(string $address): array;
+
+    /**
+     * Return assets held by the requested address.
+     *
+     * @param string $address
+     * @return array
+     */
+    public function addressAssets(string $address): array;
+
+    /**
+     * Return the UTXO input/output set for the requested address.
+     *
+     * @param string $address
+     * @param int|null $pageNo
+     * @param int|null $resultsPerPage
+     * @return array
+     */
+    public function addressUTXOs(string $address, int|null $pageNo, int|null $resultsPerPage): array;
+
+    /**
+     * Return the transactions for the requested address.
+     *
+     * @param string $address
+     * @param int|null $pageNo
+     * @param int|null $resultsPerPage
+     * @return array
+     */
+    public function addressTransactions(string $address, int|null $pageNo, int|null $resultsPerPage): array;
 }
