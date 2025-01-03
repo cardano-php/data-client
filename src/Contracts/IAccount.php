7e04331fdf01f12923fc29f7eb049060d262587a
Latheesan Kanesamoorthy <latheesan_k@hotmail.com>
2025-01-03 02:24:07 +0000

    interface Flesh out IAccount interface

--- src/Contracts/IAccount.php
+++ src/Contracts/IAccount.php
@@ -4,5 +4,81 @@ namespace CardanoPhp\DataClient;
 
 interface IAccount
 {
-    // TODO
+    /**
+     * Return information about the requested stake address.
+     *
+     * @param string $stakeAddress
+     * @return array
+     */
+    public function accountInfo(string $stakeAddress): array;
+
+    /**
+     * Return reward history for the requested stake address.
+     *
+     * @param string $stakeAddress
+     * @param int|null $pageNo
+     * @param int|null $resultsPerPage
+     * @return array
+     */
+    public function accountRewardHistory(string $stakeAddress, int|null $pageNo, int|null $resultsPerPage): array;
+
+    /**
+     * Return staking history for the requested stake address.
+     *
+     * @param string $stakeAddress
+     * @param int|null $pageNo
+     * @param int|null $resultsPerPage
+     * @return array
+     */
+    public function accountStakingHistory(string $stakeAddress, int|null $pageNo, int|null $resultsPerPage): array;
+
+    /**
+     * Return delegation history for the requested stake address.
+     *
+     * @param string $stakeAddress
+     * @param int|null $pageNo
+     * @param int|null $resultsPerPage
+     * @return array
+     */
+    public function accountDelegationHistory(string $stakeAddress, int|null $pageNo, int|null $resultsPerPage): array;
+
+    /**
+     * Return registration history for the requested stake address.
+     *
+     * @param string $stakeAddress
+     * @param int|null $pageNo
+     * @param int|null $resultsPerPage
+     * @return array
+     */
+    public function accountRegistrationHistory(string $stakeAddress, int|null $pageNo, int|null $resultsPerPage): array;
+
+    /**
+     * Return addresses (that has at least one transaction) for the requested stake address.
+     *
+     * @param string $stakeAddress
+     * @param int|null $pageNo
+     * @param int|null $resultsPerPage
+     * @return array
+     */
+    public function accountAddresses(string $stakeAddress, int|null $pageNo, int|null $resultsPerPage): array;
+
+    /**
+     * Return native assets for the requested stake address.
+     *
+     * @param string $stakeAddress
+     * @param int|null $pageNo
+     * @param int|null $resultsPerPage
+     * @return array
+     */
+    public function accountAssets(string $stakeAddress, int|null $pageNo, int|null $resultsPerPage): array;
+
+    /**
+     * Return the UTXO input/output set for the requested stake address.
+     *
+     * @param string $stakeAddress
+     * @param int|null $pageNo
+     * @param int|null $resultsPerPage
+     * @return array
+     */
+    public function accountUTXOs(string $stakeAddress, int|null $pageNo, int|null $resultsPerPage): array;
 }
