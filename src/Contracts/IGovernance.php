ca3473a29fe839d329e5a14117873003c891734a
Latheesan Kanesamoorthy <latheesan_k@hotmail.com>
2025-01-04 00:34:03 +0000

    interface Flesh out IGovernance interface

--- src/Contracts/IGovernance.php
+++ src/Contracts/IGovernance.php
@@ -4,5 +4,78 @@ namespace CardanoPhp\DataClient\Contracts;
 
 interface IGovernance
 {
-    // TODO
+    /**
+     * Return information about the requested drep.
+     *
+     * @param string $drepId
+     * @return array
+     */
+    public function governanceDRepInfo(string $drepId): array;
+
+    /**
+     * Return metadata for the requested drep.
+     *
+     * @param string $drepId
+     * @param int|null $pageNo
+     * @param int|null $resultsPerPage
+     * @return array
+     */
+    public function governanceDRepMetadata(string $drepId, int|null $pageNo, int|null $resultsPerPage): array;
+
+    /**
+     * Return certificate updates for the requested drep.
+     *
+     * @param string $drepId
+     * @param int|null $pageNo
+     * @param int|null $resultsPerPage
+     * @return array
+     */
+    public function governanceDRepUpdates(string $drepId, int|null $pageNo, int|null $resultsPerPage): array;
+
+    /**
+     * Return vote history for the requested drep.
+     *
+     * @param string $drepId
+     * @param int|null $pageNo
+     * @param int|null $resultsPerPage
+     * @return array
+     */
+    public function governanceDRepVotes(string $drepId, int|null $pageNo, int|null $resultsPerPage): array;
+
+    /**
+     * Return delegators for the requested drep.
+     *
+     * @param int|null $pageNo
+     * @param int|null $resultsPerPage
+     * @return array
+     */
+    public function governanceDRepDelegators(int|null $pageNo, int|null $resultsPerPage): array;
+
+    /**
+     * Return list of proposals.
+     *
+     * @param string $drepId
+     * @param int|null $pageNo
+     * @param int|null $resultsPerPage
+     * @return array
+     */
+    public function governanceProposals(string $drepId, int|null $pageNo, int|null $resultsPerPage): array;
+
+    /**
+     * Return information about the requested proposal.
+     *
+     * @param string $proposalTxHash
+     * @return array
+     */
+    public function governanceProposalInfo(string $proposalTxHash): array;
+
+    /**
+     * Return vote history for the requested proposal.
+     *
+     * @param string $proposalTxHash
+     * @param int|null $pageNo
+     * @param int|null $resultsPerPage
+     * @return array
+     */
+    public function governanceProposalVotes(string $proposalTxHash, int|null $pageNo, int|null $resultsPerPage): array;
 }
--- src/Contracts/IScript.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace CardanoPhp\DataClient\Contracts;
-
-interface IScript
-{
-    // TODO
-}
