5b3630408640cdd6e4fc0a62fce38cbe12ba9af5
Latheesan Kanesamoorthy <latheesan_k@hotmail.com>
2025-01-10 17:38:44 +0000

    block_dto Implement DTO classes for IBlock contract

---
+++ src/DTOs/Block/BlockInfo.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CardanoPhp\DataClient\DTOs\Block;
+
+final readonly class BlockInfo
+{
+    public function __construct(
+        public int $unixTimestamp,
+        public int $height,
+        public string $hash,
+        public int $slot,
+        public int $epoch,
+        public int $epochSlot,
+        public string $slotLeaderBech32,
+        public int $size,
+        public int $txCount,
+        public int $output,
+        public int $fees,
+        public string $blockVrfKey,
+        public string $opCertHash,
+        public string $opCertCounter,
+        public string $previousBlockHash,
+        public string $nextBlockHash,
+        public int $confirmations
+    )
+    {
+    }
+}
